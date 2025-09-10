b481bea675558da7e59c647550e3a8bd3c61ec6b
SumanthRH <sumanthrh99@gmail.com>
2025-09-10 01:40:04 +0000

    x
    Signed-off-by: SumanthRH <sumanthrh99@gmail.com>

--- skyrl-train/skyrl_train/utils/constants.py
+++ skyrl-train/skyrl_train/utils/constants.py
@@ -1,7 +1,7 @@
 import os
 
 # Timeout for allocating the placement group for different actors in SkyRL
-SKYRL_RAY_PG_TIMEOUT = int(os.environ.get("SKYRL_RAY_PG_TIMEOUT", 30))
+SKYRL_RAY_PG_TIMEOUT_IN_S = int(os.environ.get("SKYRL_RAY_PG_TIMEOUT_IN_S", 60))
 
 # export `LD_LIBRARY_PATH` to ray runtime env.
 # For some reason the `LD_LIBRARY_PATH` is not exported to the worker with .env file.
